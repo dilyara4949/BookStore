3f96309465e0991c72a289c8edd38bca87c18e45
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2023-03-30 13:12:51 +0600

    upd

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -9,10 +9,10 @@ import (
 )
 
 func Init() *gorm.DB {
-	// dbURL := "postgres://postgres:12345@localhost:5432/bookStore?sslmode=disable"
-	dbURL := "postgres://postgres:12345@localhost:5432"
-
-	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+	// dbURL := "postgres://postgres:12345@localhost:5432/BookStore?sslmode=disable"
+	// dbURL := "postgres://postgres:12345@localhost:5432"
+	
+	db, err := gorm.Open(postgres.Open("host=postgres dbname=bookStore user=postgres password=12345 sslmode=disable"), &gorm.Config{})
 
 	if err != nil {
 		log.Fatalln(err)
